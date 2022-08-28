80ecadcc3ec5347ab10b5c0ccfac2a3bc95670c1
[Mark Thomas] <[mark.i.thomas@outlook.com]>
2022-08-28 20:59:50 +0700

    Add debugging for ios

--- main.js
+++ main.js
@@ -273,7 +273,7 @@ function buildArrayAndProcess() {
 
           console.log(`This is the array member: ${ excludeArray[i]}`);
 
-          let regex3 = new RegExp(`/${(excludeArray[i]).replace("\n","")}/`);
+          let regex3 = new RegExp(`${(excludeArray[i]).replace("\n","")}`);
 
           console.log(`Using this REGEX: ${regex3}`)
 
